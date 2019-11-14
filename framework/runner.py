9c696f45b06f124d1df7991e2e188c4c9ba27fd6
Srikanth Gatta <gattasrikanth@gmail.com>
2019-11-14 11:31:21 -0500

    Create runner.py

---
+++ framework/runner.py
@@ -0,0 +1,3 @@
+# This is the runner.py usually known to be your starting point to run automation.
+# Lets start from here, and given if your task is to initially start selenium/launch browser/navigate to a website and close browser
+# Add some code here, and we can review from there.
